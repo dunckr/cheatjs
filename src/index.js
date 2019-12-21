5bf32d42ce3b7aa8199e5028dfc46a837d54e2c0
Duncan Beaton <dunckr@gmail.com>
2019-12-21 15:04:21 +0000

    working in browser..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { mocha } from 'mocha';
+import expect from 'expect';
 
-import { App } from './App';
+mocha.setup('bdd');
+// your tests here
+const source = `describe('test suite', function() {
+  it('should work', function() {
+    expect(1).toEqual(2);
+  });
+});`;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const requires = {
+  expect: expect
+};
+
+const requireKeys = Object.keys(requires);
+const requireValues = requireKeys.map(key => requires[key]);
+const fn = Function(...requireKeys, source);
+fn(...requireValues);
+
+// run tests
+mocha.run();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// import { App } from './App';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
