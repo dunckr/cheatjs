59be2ff2d83ceea24d6c6e63d532efc4ba7ca537
Duncan Beaton <dunckr@gmail.com>
2019-12-21 16:08:39 +0000

    working with mocha

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import { mocha } from 'mocha';
-import expect from 'expect';
+import * as expect from 'expect';
 
-mocha.setup('bdd');
-// your tests here
-const source = `describe('test suite', function() {
+import { Reporter } from './reporter';
+
+// mocha.setup({ ui: 'bdd' });
+mocha.setup({ ui: 'bdd', reporter: Reporter });
+
+describe('test suite', function() {
   it('should work', function() {
     expect(1).toEqual(2);
   });
-});`;
+});
 
-const requires = {
-  expect: expect
-};
-
-const requireKeys = Object.keys(requires);
-const requireValues = requireKeys.map(key => requires[key]);
-const fn = Function(...requireKeys, source);
-fn(...requireValues);
-
-// run tests
+mocha.checkLeaks();
 mocha.run();
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// import { App } from './App';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
