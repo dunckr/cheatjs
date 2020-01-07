8c55de91667537745f499391cfff1ef27288f6d2
Duncan Beaton <dunckr@gmail.com>
2020-01-07 22:06:58 +0000

    add search filter

--- src/App.js
+++ src/App.js
@@ -12,9 +12,31 @@ import {
   some
 } from './examples/Array';
 import './App.css';
+import fuzzysearch from 'fuzzysearch';
+
+const EXAMPLES = [from, every, filter, find, includes, map, reduce, some];
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      examples: EXAMPLES
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const input = event.target.value;
+    let examples = EXAMPLES;
+    if (input && input.length > 0) {
+      examples = EXAMPLES.filter(example =>
+        fuzzysearch(input.toLowerCase(), example.title.toLowerCase())
+      );
+    }
+    this.setState({ input, examples });
+  }
   render() {
+    const { input, examples } = this.state;
     return (
       <div className="wrapper">
         <h1 className="title">cheatjs.com</h1>
@@ -34,15 +56,19 @@ export class App extends React.Component {
             Star
           </a>
         </div>
-        <Environment {...from} />
-        <Environment {...every} />
-        <Environment {...filter} />
-        <Environment {...find} />
-        <Environment {...includes} />
-        <Environment {...includes} />
-        <Environment {...map} />
-        <Environment {...reduce} />
-        <Environment {...some} />
+        <div className="search">
+          <input
+            type="text"
+            name="search"
+            placeholder="Filter by method name:"
+            autoFocus="true"
+            value={input}
+            onChange={this.handleChange}
+          />
+        </div>
+        {examples.map(example => {
+          return <Environment {...example} />;
+        })}
         <div className="footer">
           <p>
             {'by '}
